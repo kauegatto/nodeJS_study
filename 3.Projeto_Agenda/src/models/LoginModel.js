323fc72ddf1e17f638ccc0ae64c99fb2d060827d
Kauê Gatto <kauegatto123@gmail.com>
2021-07-14 22:36:55 -0300

    Senha tá sendo passando pelo bcrypt e você não pode registrar um email repetido

--- 3.Projeto_Agenda/src/models/LoginModel.js
+++ 3.Projeto_Agenda/src/models/LoginModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+
 
 const LoginSchema = new mongoose.Schema({
     email: {type: String, required: true},
@@ -15,15 +17,28 @@ class Login {
         this.user = null;
     }
     async register(){
-        this.valida();
+        this.validate();
         if(this.errors.length !=0){
             return;
         }
+        await this.userExists();
+        if(this.errors.length !=0){
+            return;
+        }
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
         try{
             this.user = await LoginModel.create(this.body);
         }catch(e){console.log(e);}
     }
-    valida(){
+    async userExists(){
+        const user = await LoginModel.findOne({email:this.body.email});
+        if(user){
+            this.errors.push("Email already exists");
+        }
+    }
+    
+    validate(){
         this.cleanUp();
         //1. email válido
         if(!validator.isEmail(this.body.email)){
