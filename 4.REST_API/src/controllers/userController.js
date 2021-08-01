5f0e5eeb18ca0e05a614f9e878aa917672e621c2
kaue <kauegatto123@gmail.com>
2021-07-31 21:17:09 -0300

    error handling in userController

--- 4.REST_API/src/controllers/userController.js
+++ 4.REST_API/src/controllers/userController.js
@@ -4,15 +4,20 @@ class UserController {
   async createUser(req, res) {
     const userData = req.body;
     if (!userData.typeId) { userData.typeId = 1; }
-    // if there's no specified typeId, it's a student (unsafe for now, later we'll get a auth token)
+    // if there's no specified typeId, it's a student (unsafe for now, later we'll get an token )
     try {
       await User.create(userData).then((user) => {
         res.status(200);
         res.json(user);
       });
-    } catch {
-      res.status(404);
-      res.send();
+    } catch (e) {
+      let msgArray = [];
+      const errorArray = e.errors;
+      errorArray.forEach((error) => {
+        msgArray.push(error.message);
+      });
+      res.status(400);
+      res.send(msgArray);
     }
   }
 }
