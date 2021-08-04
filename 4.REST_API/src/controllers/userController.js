5f50ade059076bd07ab654094b924c4eebffb3e3
kaue <kauegatto123@gmail.com>
2021-08-04 19:11:28 -0300

    criado o crud do usuário, ainda tenho que implementar uma autenticação e arrumar a hash com bcrypt no bd

--- 4.REST_API/src/controllers/userController.js
+++ 4.REST_API/src/controllers/userController.js
@@ -1,7 +1,20 @@
 import User from '../models/User';
 
 class UserController {
-  async createUser(req, res) {
+  // find all
+  async findAll(req, res) {
+    try {
+      const users = await User.findAll({
+        attributes: ['name', 'email'],
+      });
+      res.json(users);
+    } catch (e) {
+      res.send('error');
+    }
+  }
+
+  // create
+  async create(req, res) {
     const userData = req.body;
     if (!userData.typeId) { userData.typeId = 1; }
     // if there's no specified typeId, it's a student (unsafe for now, later we'll get an token )
@@ -11,13 +24,68 @@ class UserController {
         res.json(user);
       });
     } catch (e) {
+      // eslint-disable-next-line prefer-const
       let msgArray = [];
       const errorArray = e.errors;
-      errorArray.forEach((error) => {
-        msgArray.push(error.message);
-      });
+      if (errorArray) { // if it's an error that comes from sequelize validation
+        errorArray.forEach((error) => {
+          msgArray.push(error.message);
+        });
+        res.status(400);
+        res.send(msgArray);
+      } else {
+        res.status(400);
+        res.json('voce:fdp');
+      }
+    }
+  }
+
+  // find one - read
+  async findOne(req, res) {
+    try {
+      const { id } = req.params;
+      console.log(req.params);
+      const user = await User.findByPk(id);
+      res.json(user);
+    } catch (e) {
+      res.send('error');
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      // req body validation;
+      if (user) {
+        await user.update(req.body).then(
+          (updatedUser) => res.send(updatedUser),
+        );
+      } else {
+        res.status(400);
+        res.json({ errors: ["user doesn't exist"] });
+      }
+    } catch (e) {
+      res.status(500);
+      res.json({ errors: ['Server errror'] });
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (user) {
+        await user.destroy().then(
+          (deleted) => res.send(deleted),
+        );
+      }
       res.status(400);
-      res.send(msgArray);
+      res.json('this user doesn\'t exist');
+    } catch (e) {
+      res.send('error');
     }
   }
 }
--- 4.REST_API/src/routes/userRouter.js
+++ 4.REST_API/src/routes/userRouter.js
@@ -3,5 +3,14 @@ import userController from '../controllers/userController';
 
 const userRouter = new Router();
 
-userRouter.post('/user/create', userController.createUser);
+userRouter.post('/user/create', userController.create);
+
+userRouter.get('/user', userController.findAll);
+userRouter.get('/user/:id', userController.findOne);
+userRouter.get('/users', userController.findAll);
+
+userRouter.put('/user/:id', userController.update);
+
+userRouter.delete('/user/:id', userController.delete);
+
 export default userRouter;
